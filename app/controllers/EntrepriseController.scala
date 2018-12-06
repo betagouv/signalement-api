947c02f5cdc1d85b73b13ec587e3af8a29e29e3e
jrivals <jerome.rivals@gmail.com>
2018-12-06 12:40:50 +0100

    Exposition de la recherche via l'API entreprise

--- app/controllers/EntrepriseController.scala
+++ app/controllers/EntrepriseController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject.Inject
 import play.api.Logger
 import play.api.libs.ws._
+import play.api.mvc.{ResponseHeader, Result}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -11,17 +12,18 @@ class EntrepriseController @Inject()(ws: WSClient)
 
   val logger: Logger = Logger(this.getClass)
 
-  def test = Action.async { implicit request =>
+  def getCompanies(name: String, city: String, maxCount: Int) = Action.async { implicit request =>
 
-    val request = ws.url("https://entreprise.data.gouv.fr/api/sirene/v1/full_text/jerome")
+    val request = ws
+      .url(s"https://entreprise.data.gouv.fr/api/sirene/v1/full_text/$name $city?per_page=$maxCount")
       .addHttpHeaders("Accept" -> "application/json", "Content-Type" -> "application/json")
 
-    request.get().flatMap( result => {
-      logger.debug(s"result ${result.json}")
-      Future(Ok(result.json))
-    }
+    request.get().flatMap(
+      response => response.status match {
+        case NOT_FOUND => Future(NotFound(response.json))
+        case _ => Future(Ok(response.json))
+      }
     );
-
 
   }
 }
