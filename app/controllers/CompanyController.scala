f94a4c98a7a1762c8a4ad98a3d92ddeb07399542
fcoufour <github@coufourier.com>
2019-03-07 09:33:48 +0100

    Chaînage des requêtes à proximité si nécessaire

--- app/controllers/CompanyController.scala
+++ app/controllers/CompanyController.scala
@@ -7,6 +7,9 @@ import play.api.mvc.{ResponseHeader, Result}
 import play.api.libs.json._
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Success, Failure}
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 case class Location(lat: Double, lon: Double)
 
@@ -39,7 +42,7 @@ class CompanyController @Inject()(ws: WSClient)
   }
 
   private def haversineDistance(lat1: Double, long1: Double, lat2: Double, long2: Double): Double = {
-    val EARTH_RADIUS = 6378137 // equatorial earth radius in meters
+    val EARTH_RADIUS = 6378137d // equatorial earth radius in meters
     val dLat = Math.toRadians(lat2 - lat1)
     val dLong = Math.toRadians(long2 - long1)
     val dLatSin = Math.sin(dLat / 2)
@@ -49,9 +52,9 @@ class CompanyController @Inject()(ws: WSClient)
     EARTH_RADIUS * c
   }
 
-  def getNearbyCompanies(lat: String, long: String, radius: Double, maxCount: Int) = Action.async { implicit request =>
+  private def getNearbyCompanies(lat: String, long: String, radius: Double, maxCount: Int, pageNumber: Int): Future[WSResponse] = {
 
-    logger.debug(s"getNearbyCompanies [$lat, $long, $radius, $maxCount]")
+    logger.debug(s"getNearbyCompanies [$lat, $long, $radius, $maxCount, $pageNumber]")
 
     var request = ws
       .url(s"https://entreprise.data.gouv.fr/api/sirene/v1/near_point/")
@@ -61,29 +64,71 @@ class CompanyController @Inject()(ws: WSClient)
     request = request.addQueryStringParameters("long" -> long)
     request = request.addQueryStringParameters("radius" -> radius.toString)
     request = request.addQueryStringParameters("per_page" -> maxCount.toString)
+    request = request.addQueryStringParameters("page" -> pageNumber.toString)
 
-    request.get().flatMap(
-      response => response.status match {
-        case NOT_FOUND => Future(NotFound(response.json))
-        case _ => {
-          Future {
-            val companies = (response.json("etablissements")).as[List[JsObject]]
-            var companiesWithDistance = new ListBuffer[JsObject]()
-            companies.foreach(company => {
-              val companyLat = (company("latitude")).as[String]
-              val companyLong = (company("longitude")).as[String]
-              val distance = haversineDistance(lat.toDouble, long.toDouble, companyLat.toDouble, companyLong.toDouble)
-              val companyWithDistance = company ++ Json.obj("distance" -> distance)
-              companiesWithDistance += companyWithDistance
-            })
-            val sortedCompanies = companiesWithDistance.sortWith((d1, d2) => d1("distance").as[Double] < d2("distance").as[Double])
-            // etablissements property is replaced by etablissement for API consistency
-            val nearbyCompaniesResponse = response.json.as[JsObject] - "etablissements" ++ Json.obj("etablissement" -> sortedCompanies)
-            Ok(nearbyCompaniesResponse)
+    request.get()
+  }
+
+  def getAllNearbyCompanies(lat: String, long: String, radius: Double, maxCount: Int) = Action.async { implicit request =>
+
+    logger.debug(s"getAllNearbyCompanies [$lat, $long, $radius, $maxCount]")
+    val startTime = System.currentTimeMillis
+
+    val MAX_COUNT = 100
+    val MAX_PAGE = 10
+
+    getNearbyCompanies(lat, long, radius, MAX_COUNT, 1).flatMap(
+      firstPage => {
+        firstPage.status match {
+          case NOT_FOUND => Future(NotFound(firstPage.json))
+          case _ => {
+            val totalResults = (firstPage.json("total_results")).as[Int]
+            logger.debug(s"totalResults $totalResults")
+            val totalPages = (firstPage.json("total_pages")).as[Int]
+            logger.debug(s"totalPages $totalPages")
+            val companies = (firstPage.json("etablissements")).as[List[JsObject]]
+            var allCompanies = companies.to[ListBuffer]
+            var nextPagesFuture = new ListBuffer[Future[WSResponse]]()
+            val numberOfPages = Math.min(totalPages, MAX_PAGE)
+            if (numberOfPages > 1) {
+              for (pageIndex <- 2 to numberOfPages) {
+                nextPagesFuture += getNearbyCompanies(lat, long, radius, MAX_COUNT, pageIndex)
+              }
+            }
+            Future.sequence(nextPagesFuture).flatMap(
+              pages => {
+                pages.foreach(page => {
+                  page.status match {
+                    case NOT_FOUND => {}
+                    case _ => {  
+                      val companies = (page.json("etablissements")).as[List[JsObject]]
+                      allCompanies ++= companies
+                    }
+                  }
+                })
+                var companiesWithDistance = new ListBuffer[JsObject]()
+                allCompanies.foreach(company => {
+                  val companyLat = (company("latitude")).as[String]
+                  val companyLong = (company("longitude")).as[String]
+                  val distance = haversineDistance(lat.toDouble, long.toDouble, companyLat.toDouble, companyLong.toDouble)
+                  val companyWithDistance = company ++ Json.obj("distance" -> distance)
+                  companiesWithDistance += companyWithDistance
+                })
+                val sortedCompanies = companiesWithDistance.sortWith((d1, d2) => d1("distance").as[Double] < d2("distance").as[Double])
+                val numberOfCompanies = sortedCompanies.length;
+                val numberOfResults = Math.min(numberOfCompanies, maxCount)
+                logger.debug(s"numberOfResults $numberOfResults")
+                val nearbyCompanies = sortedCompanies.take(numberOfResults)
+                val allNearbyCompaniesResponse = firstPage.json.as[JsObject] ++ Json.obj("total_results" -> numberOfResults) ++ Json.obj("total_pages" -> 1) ++ Json.obj("per_page" -> maxCount) - "etablissements" ++ Json.obj("etablissement" -> nearbyCompanies)
+                val duration = (System.currentTimeMillis - startTime) / 1000d
+                logger.debug(s"duration ${duration}s")
+                Future(Ok(allNearbyCompaniesResponse))
+              }
+            )
           }
         }
       }
-    );
+    )
 
   }
 
