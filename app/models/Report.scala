47925f183a6281101fec0de1fd95c6d297e00378
jrivals <jerome.rivals@gmail.com>
2019-04-05 14:17:09 +0200

    Renommage et conversion des données precision et sous-categorie pour prendre en charge des tableaux de donneés

--- app/models/Report.scala
+++ app/models/Report.scala
@@ -3,19 +3,20 @@ package models
 import java.time.{LocalDate, LocalDateTime}
 import java.util.UUID
 
+import com.github.tminglei.slickpg.composite.Struct
 import play.api.libs.json.{Json, OFormat}
 
 case class Report(
                    id: Option[UUID],
                    category: String,
-                   subcategory: Option[String],
-                   precision: Option[String],
+                   subcategories: List[String],
+                   details: List[DetailInputValue],
                    companyName: String,
                    companyAddress: String,
                    companyPostalCode: Option[String],
                    companySiret: Option[String],
                    creationDate: Option[LocalDateTime],
-                   anomalyDate: LocalDate,
+                   anomalyDate: Option[LocalDate],
                    anomalyTimeSlot: Option[Int],
                    description: Option[String],
                    firstName: String,
@@ -26,5 +27,16 @@ case class Report(
                  )
 object Report {
   implicit val reportFormat: OFormat[Report] = Json.format[Report]
+
+}
+
+
+case class  DetailInputValue (
+                           label: String,
+                           value: String
+                 ) extends Struct
+
+object DetailInputValue {
+  implicit val detailInputValueFormat: OFormat[DetailInputValue] = Json.format[DetailInputValue]
 
 }
