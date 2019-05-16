952275513c2a724cc4635e9213df22014214c903
jrivals <jerome.rivals@gmail.com>
2019-05-16 13:37:27 +0200

    Test scripts SQL d'insertion pour Heroku

---
+++ test/scripts/insert_users.sql
@@ -0,0 +1,1 @@
+INSERT INTO USERS VALUES ('e6de6b48-1c53-4d3e-a7ff-dd9b643073cf', 'test.admin@signalconso.beta.gouv.fr', '$2a$10$z5OdWpY05I9tvCJt0EplWOqLWLi6TFCyzZA4DoVlvCWbjmgCsQzwC', 'Test', 'Admin', 'Admin')
