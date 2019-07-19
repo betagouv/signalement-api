828ffe8ea721fdabe944b133922f1b702cd8036a
jrivals <jerome.rivals@gmail.com>
2019-07-19 11:59:06 +0200

    Ajout API pour évaluation des informations

---
+++ app/controllers/RatingController.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+import com.mohiva.play.silhouette.api.Silhouette
+import javax.inject._
+import models.Rating
+import play.api.Logger
+import play.api.libs.json.{JsError, Json}
+import repositories.RatingRepository
+import utils.silhouette.AuthEnv
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+@Singleton
+class RatingController @Inject()(ratingRepository: RatingRepository,
+                                 val silhouette: Silhouette[AuthEnv])(implicit ec: ExecutionContext) extends BaseController {
+
+  val logger: Logger = Logger(this.getClass)
+
+  def rate = UserAwareAction.async(parse.json) { implicit request =>
+
+    logger.debug("rate")
+
+    request.body.validate[Rating].fold(
+      errors => Future.successful(BadRequest(JsError.toJson(errors))),
+      rating => {
+        ratingRepository.createRating(
+          rating.copy(
+            id = Some(UUID.randomUUID()),
+            creationDate = Some(LocalDateTime.now()))
+        ).flatMap(rating => Future.successful(Ok(Json.toJson(rating))))
+      }
+    )
+  }
+}
---
+++ app/repositories/RatingRepository.scala
@@ -0,0 +1,54 @@
+package repositories
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+import javax.inject.{Inject, Singleton}
+import models.Rating
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+@Singleton
+class RatingRepository @Inject()(dbConfigProvider: DatabaseConfigProvider, reportRepository: ReportRepository)(implicit ec: ExecutionContext) {
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import PostgresProfile.api._
+  import dbConfig._
+
+  private class RatingTable(tag: Tag) extends Table[Rating](tag, "ratings") {
+
+    def id = column[UUID]("id", O.PrimaryKey)
+    def creationDate = column[LocalDateTime]("creation_date")
+    def category = column[String]("category")
+    def subcategories = column[List[String]]("subcategories")
+    def positive = column[Boolean]("positive")
+
+    type RatingData = (UUID, LocalDateTime, String, List[String], Boolean)
+
+    def constructRating: RatingData => Rating = {
+
+      case (id, creationDate, category, subcategories, positive) => {
+        Rating(Some(id), Some(creationDate), category, subcategories, positive)
+      }
+    }
+
+    def extractRating: PartialFunction[Rating, RatingData] = {
+      case Rating(id, creationDate, category, subcategories, positive) => (id.get, creationDate.get, category, subcategories, positive)
+    }
+
+    def * =
+      (id, creationDate, category, subcategories, positive) <> (constructRating, extractRating.lift)
+  }
+
+  private val ratingTableQuery = TableQuery[RatingTable]
+
+  def createRating(rating: Rating): Future[Rating] = db
+    .run(ratingTableQuery += rating)
+    .map(_ => rating)
+
+}
+
