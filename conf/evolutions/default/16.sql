ceb9f2656ba251b691279e45af77857094e3b2f3
jrivals <jerome.rivals@gmail.com>
2019-09-11 14:17:03 +0200

    Test drop index avant alter table

--- conf/evolutions/default/16.sql
+++ conf/evolutions/default/16.sql
@@ -1,7 +1,15 @@
 # --- !Ups
 
+drop index no_similar_report;
+
 ALTER TABLE signalement ALTER date_creation TYPE timestamptz USING date_creation AT TIME ZONE 'UTC';
+
+create unique index no_similar_report on signalement (email, nom, prenom, details, date_trunc('day'::text, date_creation), adresse_etablissement);
 
 # --- !Downs
 
+drop index no_similar_report;
+
 ALTER TABLE signalement ALTER date_creation TYPE timestamp;
+
+create unique index no_similar_report on signalement (email, nom, prenom, details, date_trunc('day'::text, date_creation), adresse_etablissement);
