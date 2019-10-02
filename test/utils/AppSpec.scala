779d93c58e7b5dcb2a8da4e582c64c47c25cb4f9
Thomas Chaumeny <thomas.chaumeny@beta.gouv.fr>
2019-10-02 12:22:33 +0200

    Third try...

--- test/utils/AppSpec.scala
+++ test/utils/AppSpec.scala
@@ -1,35 +1,24 @@
 package utils
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
-
 import play.Application
-import play.api.Mode
-import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.DBApi
+import play.api.db.evolutions._
 import play.inject.guice.GuiceApplicationBuilder
 
-import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
-
-import org.specs2.Spec
-import org.specs2.execute.{AsResult, Result}
 import org.specs2.specification._
 
-trait AppSpec extends AroundEach with BeforeAll {
+trait AppSpec extends BeforeAfterAll {
   lazy val app = new GuiceApplicationBuilder().build()
   def injector = app.asScala.injector
-  lazy val dbConfig = injector.instanceOf[DatabaseConfigProvider].get[JdbcProfile]
+  private lazy val database = injector.instanceOf[DBApi].database("default")
 
   def setupData() {}
 
   def beforeAll(): Unit = {
-    setupData
+    Evolutions.applyEvolutions(database)
+    setupData()
   }
-  def around[R: AsResult](r: => R): Result = {
-    import dbConfig.driver.api._
-    val timeout = 1.second
-    Await.result(dbConfig.db.run(sql"BEGIN".as[String]), timeout)
-    try AsResult(r)
-    finally Await.result(dbConfig.db.run(sql"ROLLBACK".as[String]), timeout)
+  def afterAll(): Unit = {
+    Evolutions.cleanupEvolutions(database)
   }
 }
