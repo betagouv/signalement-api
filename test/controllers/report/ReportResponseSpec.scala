9dfa69d1c4179b32e7d48fb90988af99e05a2a3e
jrivals <jerome.rivals@gmail.com>
2019-12-18 11:11:14 +0100

    Fix tests

--- test/controllers/report/ReportResponseSpec.scala
+++ test/controllers/report/ReportResponseSpec.scala
@@ -54,6 +54,7 @@ class ReportResponseProAnswer(implicit ee: ExecutionEnv) extends ReportResponseS
         Given an authenticated pro user which is concerned by the report         ${step(someLoginInfo = Some(concernedProLoginInfo))}
         When post a response with type "ACCEPTED"                                ${step(someResult = Some(postReportResponse(reportResponseAccepted)))}
         Then an event "REPONSE_PRO_SIGNALEMENT" is created                       ${eventMustHaveBeenCreatedWithAction(ActionEvent.REPONSE_PRO_SIGNALEMENT)}
+        And an event "EMAIL_REPONSE_PRO" is created                              ${eventMustHaveBeenCreatedWithAction(ActionEvent.EMAIL_REPONSE_PRO)}
         And the response files are attached to the report                        ${reportFileMustHaveBeenAttachedToReport()}
         And the report reportStatusList is updated to "PROMESSE_ACTION"          ${reportMustHaveBeenUpdatedWithStatus(reportUUID, ReportStatus.PROMESSE_ACTION)}
         And an acknowledgment email is sent to the consumer                      ${mailMustHaveBeenSent(reportFixture.email,"Le professionnel a répondu à votre signalement", views.html.mails.consumer.reportToConsumerAcknowledgmentPro(report, reportResponseAccepted).toString, Seq(AttachmentFile("logo-signal-conso.png", app.environment.getFile("/appfiles/logo-signal-conso.png"), contentId = Some("logo"))))}
@@ -68,6 +69,7 @@ class ReportResponseProRejectedAnswer(implicit ee: ExecutionEnv) extends ReportR
         Given an authenticated pro user which is concerned by the report         ${step(someLoginInfo = Some(concernedProLoginInfo))}
         When post a response with type "REJECTED"                                ${step(someResult = Some(postReportResponse(reportResponseRejected)))}
         Then an event "REPONSE_PRO_SIGNALEMENT" is created                       ${eventMustHaveBeenCreatedWithAction(ActionEvent.REPONSE_PRO_SIGNALEMENT)}
+        And an event "EMAIL_REPONSE_PRO" is created                              ${eventMustHaveBeenCreatedWithAction(ActionEvent.EMAIL_REPONSE_PRO)}
         And the report reportStatusList is updated to "SIGNALEMENT_INFONDE"      ${reportMustHaveBeenUpdatedWithStatus(reportUUID, ReportStatus.SIGNALEMENT_INFONDE)}
         And an acknowledgment email is sent to the consumer                      ${mailMustHaveBeenSent(reportFixture.email,"Le professionnel a répondu à votre signalement", views.html.mails.consumer.reportToConsumerAcknowledgmentPro(report, reportResponseRejected).toString, Seq(AttachmentFile("logo-signal-conso.png", app.environment.getFile("/appfiles/logo-signal-conso.png"), contentId = Some("logo"))))}
         And an acknowledgment email is sent to the professional                  ${mailMustHaveBeenSent(concernedProUser.email.get,"Votre réponse au signalement", views.html.mails.professional.reportAcknowledgmentPro(reportResponseRejected, concernedProUser).toString, Seq(AttachmentFile("logo-signal-conso.png", app.environment.getFile("/appfiles/logo-signal-conso.png"), contentId = Some("logo"))))}
@@ -81,6 +83,7 @@ class ReportResponseProNotConcernedAnswer(implicit ee: ExecutionEnv) extends Rep
         Given an authenticated pro user which is concerned by the report         ${step(someLoginInfo = Some(concernedProLoginInfo))}
         When post a response with type "NOT_CONCERNED"                           ${step(someResult = Some(postReportResponse(reportResponseNotConcerned)))}
         Then an event "REPONSE_PRO_SIGNALEMENT" is created                       ${eventMustHaveBeenCreatedWithAction(ActionEvent.REPONSE_PRO_SIGNALEMENT)}
+        And an event "EMAIL_REPONSE_PRO" is created                              ${eventMustHaveBeenCreatedWithAction(ActionEvent.EMAIL_REPONSE_PRO)}
         And the report reportStatusList is updated to "MAL_ATTRIBUE"             ${reportMustHaveBeenUpdatedWithStatus(reportUUID, ReportStatus.SIGNALEMENT_MAL_ATTRIBUE)}
         And an acknowledgment email is sent to the consumer                      ${mailMustHaveBeenSent(reportFixture.email,"Le professionnel a répondu à votre signalement", views.html.mails.consumer.reportToConsumerAcknowledgmentPro(report, reportResponseNotConcerned).toString, Seq(AttachmentFile("logo-signal-conso.png", app.environment.getFile("/appfiles/logo-signal-conso.png"), contentId = Some("logo"))))}
         And an acknowledgment email is sent to the professional                  ${mailMustHaveBeenSent(concernedProUser.email.get,"Votre réponse au signalement", views.html.mails.professional.reportAcknowledgmentPro(reportResponseNotConcerned, concernedProUser).toString, Seq(AttachmentFile("logo-signal-conso.png", app.environment.getFile("/appfiles/logo-signal-conso.png"), contentId = Some("logo"))))}
@@ -190,8 +193,7 @@ abstract class ReportResponseSpec(implicit ee: ExecutionEnv) extends Specificati
 
   def eventMustHaveBeenCreatedWithAction(action: ActionEventValue) = {
     val events = Await.result(eventRepository.list, Duration.Inf).toList
-    events.length must beEqualTo(1)
-    events.head.action must beEqualTo(action)
+    events.map(_.action) must contain(action)
   }
 
   def eventActionMatcher(action: ActionEventValue): org.specs2.matcher.Matcher[Event] = { event: Event =>
