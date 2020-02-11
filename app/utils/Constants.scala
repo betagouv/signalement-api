600c587b7d3691870eb84cef44db24f74f5434bc
jrivals <jerome.rivals@gmail.com>
2020-02-11 09:20:12 +0100

    Limit service to experiment departments

--- app/utils/Constants.scala
+++ app/utils/Constants.scala
@@ -246,7 +246,7 @@ object Constants {
      GrandEst ++ Guadeloupe ++ Guyane ++ HautsDeFrance ++ IleDeFrance ++ LaReunion ++ Martinique ++ Mayotte ++
       Normandie ++ NouvelleAquitaine ++ OCC ++ PaysDeLaLoire ++ ProvenceAlpesCoteAzur
 
-    val AUTHORIZED = ALL
+    val AUTHORIZED = AURA ++ CDVL ++ OCC
   }
 
 }
