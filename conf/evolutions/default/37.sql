f596fe42929d9aaa254a31695af9d5ca4f1924c3
jrivals <jerome.rivals@gmail.com>
2020-03-26 09:20:40 +0100

    Refactor and restrict creation of report events

--- conf/evolutions/default/37.sql
+++ conf/evolutions/default/37.sql
@@ -1,7 +1,7 @@
 -- !Ups
 
-ALTER TABLE events SET event_type = 'ADMIN' where event_type = 'RECTIF';
-ALTER TABLE events SET event_type = 'DGCCRF', action = 'Ajout d''un commentaire' where action = 'Ajout d''un commentaire interne à la DGCCRF';
+UPDATE events SET event_type = 'ADMIN' where event_type = 'RECTIF';
+UPDATE events SET event_type = 'DGCCRF', action = 'Ajout d''un commentaire' where action = 'Ajout d''un commentaire interne à la DGCCRF';
 
 -- !Downs
 
