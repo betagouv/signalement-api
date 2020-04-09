21e6213ca90800147291a1c43d828995248f2919
Thomas Chaumeny <thomas.chaumeny@beta.gouv.fr>
2020-04-09 10:56:22 +0200

    Squash migrations

--- conf/evolutions/default/41.sql
+++
@@ -1,7 +0,0 @@
--- !Ups
-
-ALTER TABLE events ALTER COLUMN report_id DROP NOT NULL;
-
--- !Downs
-
-ALTER TABLE events ALTER COLUMN report_id SET NOT NULL;
--- conf/evolutions/default/42.sql
+++
@@ -1,9 +0,0 @@
--- !Ups
-
-ALTER TABLE access_tokens ADD COLUMN creation_date TIMESTAMPTZ;
-UPDATE access_tokens SET creation_date = expiration_date - INTERVAL '60 days' WHERE creation_date IS NULL;
-ALTER TABLE access_tokens ALTER COLUMN creation_date SET NOT NULL;
-
--- !Downs
-
-ALTER TABLE access_tokens DROP COLUMN creation_date;
