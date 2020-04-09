6d181182755484938254372e334efbc370273b57
Thomas Chaumeny <thomas.chaumeny@beta.gouv.fr>
2020-04-09 12:00:14 +0200

    Do not set back NOT NULL in down migration

--- conf/evolutions/default/40.sql
+++ conf/evolutions/default/40.sql
@@ -10,5 +10,4 @@ ALTER TABLE access_tokens ALTER COLUMN creation_date SET NOT NULL;
 
 -- !Downs
 
-ALTER TABLE events ALTER COLUMN report_id SET NOT NULL;
 ALTER TABLE access_tokens DROP COLUMN creation_date;
