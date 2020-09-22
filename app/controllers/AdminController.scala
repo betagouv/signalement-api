ec489a7f147c8b579c132c2814b41e57e4117812
Thomas Chaumeny <thomas.chaumeny@beta.gouv.fr>
2020-09-22 17:32:08 +0200

    Add a few emails

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -13,7 +13,7 @@ import models._
 import play.api.{Configuration, Logger}
 import repositories._
 import utils.silhouette.auth.{AuthEnv, WithPermission, WithRole}
-import utils.EmailAddress
+import utils.{Address, EmailAddress, SIRET}
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
@@ -31,6 +31,8 @@ class AdminController @Inject()(reportRepository: ReportRepository,
   implicit val websiteUrl = configuration.get[URI]("play.website.url")
   val mailFrom = configuration.get[EmailAddress]("play.mail.from")
   implicit val timeout: akka.util.Timeout = 5.seconds
+
+  val dummyURL = java.net.URI.create("https://lien-test")
 
   private def genReport = Report(
     id = UUID.randomUUID,
@@ -53,9 +55,21 @@ class AdminController @Inject()(reportRepository: ReportRepository,
     status = utils.Constants.ReportStatus.TRAITEMENT_EN_COURS
   )
 
+  private def genCompany = Company(
+    id = UUID.randomUUID,
+    siret = SIRET("123456789"),
+    creationDate = OffsetDateTime.now,
+    name = "Test Entreprise",
+    address = Address("3 rue des Champs 75015 Paris"),
+    postalCode = Some("75015")
+  )
+
   private def genBody(templateRef: String) = {
     templateRef match {
-      case "report_notification" => Some(views.html.mails.professional.reportNotification(genReport))
+      case "admin_report_notification" => Some(views.html.mails.professional.reportNotification(genReport))
+      case "consumer_report_ack" => Some(views.html.mails.consumer.reportAcknowledgment(genReport, Nil))
+      case "pro_report_notification" => Some(views.html.mails.professional.reportNotification(genReport))
+      case "pro_access_invitation" => Some(views.html.mails.professional.companyAccessInvitation(dummyURL, genCompany, None))
       case _ => None
     }
   }
@@ -65,7 +79,7 @@ class AdminController @Inject()(reportRepository: ReportRepository,
       emailActor ? EmailActor.EmailRequest(
           from = mailFrom,
           recipients = Seq(EmailAddress(to)),
-          subject = "Nouveau signalement",
+          subject = "Email de test",  // FIXME genBody should also return the subject once we use constants
           bodyHtml = body.toString
       )
     ).map(_ => Ok).getOrElse(NotFound))
