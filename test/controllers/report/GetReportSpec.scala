ecce214ccb5273bad47feffd55ecfe48e6bd5db5
jrivals <jerome.rivals@gmail.com>
2020-10-06 16:07:49 +0200

    Fix tests

--- test/controllers/report/GetReportSpec.scala
+++ test/controllers/report/GetReportSpec.scala
@@ -192,19 +192,19 @@ trait GetReportContext extends Mockito {
   val neverRequestedReportUUID = UUID.randomUUID
   val neverRequestedReport = Report(
     neverRequestedReportUUID, "category", List("subcategory"), List(), Some(companyId), Some("companyName"), Some(Address("companyAddress")), Some(Departments.ALL(0)),
-    Some(siretForConcernedPro), None, None, OffsetDateTime.now(), "firstName", "lastName", EmailAddress("email"), true, false, TRAITEMENT_EN_COURS, None
+    Some(siretForConcernedPro), None, None, OffsetDateTime.now(), "firstName", "lastName", EmailAddress("email"), true, false, TRAITEMENT_EN_COURS
   )
 
   val neverRequestedFinalReportUUID = UUID.randomUUID();
   val neverRequestedFinalReport = Report(
     neverRequestedFinalReportUUID, "category", List("subcategory"), List(), Some(companyId), Some("companyName"), Some(Address("companyAddress")), Some(Departments.ALL(0)),
-    Some(siretForConcernedPro), None, None, OffsetDateTime.now(), "firstName", "lastName", EmailAddress("email"), true, false, SIGNALEMENT_CONSULTE_IGNORE, None
+    Some(siretForConcernedPro), None, None, OffsetDateTime.now(), "firstName", "lastName", EmailAddress("email"), true, false, SIGNALEMENT_CONSULTE_IGNORE
   )
 
   val alreadyRequestedReportUUID = UUID.randomUUID();
   val alreadyRequestedReport = Report(
     alreadyRequestedReportUUID, "category", List("subcategory"), List(), Some(companyId), Some("companyName"), Some(Address("companyAddress")), Some(Departments.ALL(0)),
-    Some(siretForConcernedPro), None, None, OffsetDateTime.now(), "firstName", "lastName", EmailAddress("email"), true, false, SIGNALEMENT_TRANSMIS, None
+    Some(siretForConcernedPro), None, None, OffsetDateTime.now(), "firstName", "lastName", EmailAddress("email"), true, false, SIGNALEMENT_TRANSMIS
   )
 
   val adminUser = Fixtures.genAdminUser.sample.get
