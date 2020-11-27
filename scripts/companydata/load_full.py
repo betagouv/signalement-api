302882a42b5ef021dd2bc81f3798e2fef759d300
jrivals <jerome.rivals@gmail.com>
2020-11-27 09:23:00 +0100

    Try bulk insert/update to improve performances

--- scripts/companydata/load_full.py
+++ scripts/companydata/load_full.py
@@ -49,7 +49,7 @@ def run(pg_uri, source_csv):
 
     data = [{
                 **line,
-            } for line in iter_queries(source_csv) if d['denominationusuelleetablissement'] ]
+            } for line in iter_queries(source_csv) if line['denominationusuelleetablissement'] ]
 
     print(data)
 
