ff51b228a56a443b04dc3d7056ffda0cf70949d0
jrivals <jerome.rivals@gmail.com>
2020-11-27 11:53:57 +0100

    Try to fix out of memory bug

--- scripts/companydata/load_full_batch.py
+++ scripts/companydata/load_full_batch.py
@@ -49,7 +49,7 @@ def eval_query():
 def run(pg_uri, source_csv):
     conn = psycopg2.connect(pg_uri)
     conn.set_session(autocommit=True)
-    cur = conn.cursor()
+    cur = conn.cursor("updates_cursor")
 
     print(datetime.now())
 
