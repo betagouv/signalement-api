6ec0af12f2ace8b4aee918f7ac76107fa24868f0
jrivals <jerome.rivals@gmail.com>
2020-11-27 13:41:53 +0100

    Try to fix out of memory bug

--- scripts/companydata/load_full_batch.py
+++ scripts/companydata/load_full_batch.py
@@ -33,7 +33,7 @@ def iter_queries(path):
             elif args.type == SIREN:
                 d['denominationusuelleetablissement'] = d['denominationunitelegale'] or d['denominationusuelle1unitelegale'] or d['denominationusuelle2unitelegale'] or d['denominationusuelle3unitelegale'] or (d['prenomusuelunitelegale'] + ' ' + d['nomusageunitelegale'])
                 updates = OrderedDict((k, v) for k, v in d.items() if k in FIELDS and isset(v))
-            yield updates.items()
+            yield list(updates.items())
         else:
             break
 
