b707baa3f9214d577ccf3eefa06d17a72da3c4cf
jrivals <jerome.rivals@gmail.com>
2020-11-27 13:53:22 +0100

    Try to fix out of memory bug

--- scripts/companydata/load_full_batch.py
+++ scripts/companydata/load_full_batch.py
@@ -32,7 +32,8 @@ def iter_queries(path):
                 updates = d #OrderedDict((k, v) for k, v in d.items())
             elif args.type == SIREN:
                 d['denominationusuelleetablissement'] = d['denominationunitelegale'] or d['denominationusuelle1unitelegale'] or d['denominationusuelle2unitelegale'] or d['denominationusuelle3unitelegale'] or (d['prenomusuelunitelegale'] + ' ' + d['nomusageunitelegale'])
-                updates = [(k, v) for k, v in d.items() if k in FIELDS and isset(v)]
+                if isset(d['denominationusuelleetablissement']):
+                    updates = d
             yield updates
         else:
             break
@@ -57,6 +58,8 @@ def run(pg_uri, source_csv):
     cur = conn.cursor()
 
     print(datetime.now())
+
+    print(iter_queries(source_csv))
 
     #    data = [{ **line } for line in iter_queries(source_csv) if 'denominationusuelleetablissement' in line.keys() ]
     #psycopg2.extras.execute_batch(cur, filter(lambda elem: 'denominationusuelleetablissement' in elem.keys(), iter_queries(source_csv).items()), data, page_size = PAGE_SIZE)
