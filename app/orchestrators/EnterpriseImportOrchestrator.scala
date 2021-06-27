a32d6f23104cabff11c0c3b5a8e8719678996c86
Said SEDOUD <said.sedoud@ingenico.com>
2021-06-27 15:56:34 +0200

    Revert "close akka stream instead of filestream"
    This reverts commit c86691030629dedf71940930e3727cb349242700.

--- app/orchestrators/EnterpriseImportOrchestrator.scala
+++ app/orchestrators/EnterpriseImportOrchestrator.scala
@@ -74,7 +74,10 @@ class EnterpriseImportOrchestrator @Inject()(
         activitePrincipaleEtablissement = getValue("activiteprincipaleetablissement").getOrElse(""),
         etatAdministratifEtablissement = getValue("etatadministratifetablissement"),
       )),
-    action = companyDataRepository.insertAllRaw
+    action = companyDataRepository.insertAllRaw,
+    // TODO Don't use because cancelling the stream will tigger it.
+    //  I need a way to only call onEnd only when the stream is completed.
+    // onEnd = () => enterpriseActor ? startUniteLegaleFileActor,
   )
 
   private[this] lazy val startUniteLegaleFileActor = EnterpriseSyncActor.Start(
