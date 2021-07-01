dcefe315f2ac5cf94ab29e8fdc0ac0b971752e61
Alexandre Annic <alexandre.annic1@gmail.com>
2021-07-01 08:32:01 +0200

    Clean evolutions

--- conf/evolutions/default/56.sql
+++ conf/evolutions/default/56.sql
@@ -48,27 +48,3 @@ ALTER TABLE reports ALTER COLUMN company_postal_code DROP NOT NULL;
 
 ALTER TABLE reports DROP COLUMN done;
 ALTER TABLE reports DROP COLUMN done;
---
--- -- !Ups
---
--- ALTER TABLE companies RENAME COLUMN address TO address_old_version;
--- ALTER TABLE companies RENAME COLUMN postal_code TO postal_code_old_version;
--- ALTER TABLE companies RENAME COLUMN department TO department_old_version;
--- ALTER TABLE reports RENAME COLUMN company_address TO company_address_old_version;
--- ALTER TABLE reports RENAME COLUMN company_postal_code TO company_postal_code_old_version;
--- ALTER TABLE reports RENAME COLUMN company_country TO company_country_old_version;
---
--- ALTER TABLE companies ADD COLUMN done BOOLEAN;
--- ALTER TABLE reports ADD COLUMN done BOOLEAN;
---
--- -- !Downs
---
--- ALTER TABLE companies RENAME COLUMN address_old_version TO address;
--- ALTER TABLE companies RENAME COLUMN postal_code_old_version TO postal_code;
--- ALTER TABLE companies RENAME COLUMN department_old_version TO department;
--- ALTER TABLE reports RENAME COLUMN company_address_old_version TO company_address;
--- ALTER TABLE reports RENAME COLUMN company_postal_code_old_version TO company_postal_code;
--- ALTER TABLE reports RENAME COLUMN company_country_old_version TO company_country;
---
--- ALTER TABLE reports DROP COLUMN done;
--- ALTER TABLE reports DROP COLUMN done;
