2c1160e6e388f1f0fde26eea19c47a998c7daa60
Alexandre Annic <alexandre.annic1@gmail.com>
2021-09-20 15:59:26 +0200

    Clean
    CLean

--- app/models/SearchCompanyIdentity.scala
+++ app/models/SearchCompanyIdentity.scala
@@ -1,17 +1,25 @@
 package models
 
 import java.util.UUID
+import scala.util.Failure
+import scala.util.Success
 import scala.util.Try
-
-trait SearchCompanyIdentity {
-  val value: String
-}
+trait SearchCompanyIdentity
 
 case class SearchCompanyIdentityRCS(value: String) extends SearchCompanyIdentity
 case class SearchCompanyIdentitySiret(value: String) extends SearchCompanyIdentity
 case class SearchCompanyIdentitySiren(value: String) extends SearchCompanyIdentity
 case class SearchCompanyIdentityName(value: String) extends SearchCompanyIdentity
-case class SearchCompanyIdentityId(value: String) extends SearchCompanyIdentity
+case class SearchCompanyIdentityId(value: UUID) extends SearchCompanyIdentity
+
+object SearchCompanyIdentityId {
+
+  def unapply(value: String): Option[UUID] =
+    Try(UUID.fromString(value)) match {
+      case Success(uuid) => Some(uuid)
+      case Failure(_)    => None
+    }
+}
 
 object SearchCompanyIdentity {
   def fromString(identity: String): SearchCompanyIdentity = {
@@ -20,8 +28,8 @@ object SearchCompanyIdentity {
       case q if q.matches("[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}") => SearchCompanyIdentityRCS(q.toLowerCase())
       case q if q.matches("[0-9]{14}")                     => SearchCompanyIdentitySiret(q)
       case q if q.matches("[0-9]{9}")                      => SearchCompanyIdentitySiren(q)
-      case q if Try(UUID.fromString(q)).isSuccess          => SearchCompanyIdentityId(q)
-      case q                                               => SearchCompanyIdentityName(identity)
+      case SearchCompanyIdentityId(uuid)                   => SearchCompanyIdentityId(uuid)
+      case _                                               => SearchCompanyIdentityName(identity)
     }
   }
 }
