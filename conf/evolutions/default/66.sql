02527afdcf6e26e2b39aefc40cbe0cf4ed58142d
Alexandre Annic <alexandre.annic1@gmail.com>
2021-11-29 15:04:31 +0100

    Remove db columns related to address migration

---
+++ conf/evolutions/default/66.sql
@@ -0,0 +1,12 @@
+-- !Ups
+
+ALTER TABLE companies DROP COLUMN address_old_version;
+ALTER TABLE companies DROP COLUMN postal_code_old_version;
+ALTER TABLE companies DROP COLUMN department_old_version;
+ALTER TABLE companies DROP COLUMN done;
+
+ALTER TABLE reports DROP COLUMN company_address_old_version;
+ALTER TABLE reports DROP COLUMN company_postal_code_old_version;
+ALTER TABLE reports DROP COLUMN done;
+
+-- !Downs
