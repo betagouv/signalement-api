d567b3e36d9510bba0c5b06ba8f6fc220fd2b5e0
Alexandre Annic <alexandre.annic1@gmail.com>
2021-11-29 15:40:51 +0100

    Stop using _old and Add downs

--- conf/evolutions/default/66.sql
+++ conf/evolutions/default/66.sql
@@ -10,3 +10,12 @@ ALTER TABLE reports DROP COLUMN company_postal_code_old_version;
 ALTER TABLE reports DROP COLUMN done;
 
 -- !Downs
+
+ALTER TABLE companies ADD COLUMN address_old_version VARCHAR;
+ALTER TABLE companies ADD COLUMN postal_code_old_version VARCHAR;
+ALTER TABLE companies ADD COLUMN department_old_version;
+ALTER TABLE companies ADD COLUMN done VARCHAR;
+
+ALTER TABLE reports ADD COLUMN company_address_old_version VARCHAR;
+ALTER TABLE reports ADD COLUMN company_postal_code_old_version VARCHAR;
+ALTER TABLE reports ADD COLUMN done VARCHAR;
