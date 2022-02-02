65ae4c453361e8f293bff40570dcc7ee4b8611f0
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-02-02 15:12:26 +0100

    updating script

--- conf/evolutions/default/70.sql
+++ conf/evolutions/default/70.sql
@@ -7,7 +7,7 @@ update reports set tags = array_replace(tags, 'Démarchage à domicile','Demarch
 update reports set tags = array_replace(tags, 'Démarchage téléphonique','DemarchageTelephonique');
 update reports set tags = array_replace(tags, 'Absence de médiateur','AbsenceDeMediateur');
 update reports set tags = array_replace(tags, 'Produit industriel','ProduitIndustriel');
-update reports set tags = array_replace(tags, 'Produit alimentaire','ProduitAlimentaire');
+update reports set tags = array_replace(tags, 'Produit Alimentaire','ProduitAlimentaire');
 update reports set tags = array_replace(tags, 'Compagnie aerienne','CompagnieAerienne');
 
 -- !Downs
@@ -19,5 +19,5 @@ update reports set tags = array_replace(tags, 'DemarchageADomicile','Démarchage
 update reports set tags = array_replace(tags, 'DemarchageTelephonique','Démarchage téléphonique');
 update reports set tags = array_replace(tags, 'AbsenceDeMediateur','Absence de médiateur');
 update reports set tags = array_replace(tags, 'ProduitIndustriel','Produit industriel');
-update reports set tags = array_replace(tags, 'ProduitAlimentaire','Produit alimentaire');
+update reports set tags = array_replace(tags, 'ProduitAlimentaire','Produit Alimentaire');
 update reports set tags = array_replace(tags, 'CompagnieAerienne','Compagnie aerienne');
