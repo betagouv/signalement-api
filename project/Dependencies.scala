96ee94c2c16f141b123779177bade7e7e1a3e71a
Scala Steward <scala_steward@virtuslab.com>
2022-07-05 17:54:54 +0000

    Update postgresql to 42.3.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     lazy val playJsonExtensionsVersion = "0.42.0"
     lazy val awsJavaSdkS3Version = "1.12.207"
     lazy val jacksonModuleScalaVersion = "2.13.2"
-    lazy val postgresqlVersion = "42.3.4"
+    lazy val postgresqlVersion = "42.3.6"
     lazy val refindedVersion = "0.9.28"
     lazy val ficusVersion = "1.5.2"
     lazy val spoiwoVersion = "2.2.1"
