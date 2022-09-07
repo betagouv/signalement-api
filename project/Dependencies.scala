64db6942f993f4a49f5e859617cba04ec904c082
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 13:52:10 +0000

    Update jackson-module-scala to 2.13.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     lazy val pureConfigVersion = "0.17.1"
     lazy val playJsonExtensionsVersion = "0.42.0"
     lazy val awsJavaSdkS3Version = "1.12.259"
-    lazy val jacksonModuleScalaVersion = "2.13.3"
+    lazy val jacksonModuleScalaVersion = "2.13.4"
     lazy val postgresqlVersion = "42.3.6"
     lazy val refinedVersion = "0.9.29"
     lazy val ficusVersion = "1.5.2"
