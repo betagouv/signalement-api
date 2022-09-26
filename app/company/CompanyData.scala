f8e6352d8f14e74a76f52110a695fde80a2c4ea9
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-09-26 15:12:43 +0200

    make malformed request logged as error

--- app/company/CompanyData.scala
+++ app/company/CompanyData.scala
@@ -80,7 +80,7 @@ case class CompanySearchResult(
     activityLabel: Option[String],
     isMarketPlace: Boolean = false,
     isOpen: Boolean,
-    isPublic: Boolean = false
+    isPublic: Boolean
 )
 
 object CompanySearchResult {
