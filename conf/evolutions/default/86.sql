1254d1b364923b5ac02d02c8d666a5d087ae8e58
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-10-15 10:13:28 +0200

    TRELLO-1442: fix test

--- conf/evolutions/default/86.sql
+++ conf/evolutions/default/86.sql
@@ -1,6 +1,6 @@
 -- !Ups
 
-CREATE TABLE company_sync
+CREATE TABLE IF NOT EXISTS company_sync
 (
     id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
     last_updated TIMESTAMP not null default '1970-01-01'::timestamp
