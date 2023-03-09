7c749e59b50710c163dfbd421b30ae666cc27fc1
Charles Dufour <charles.dufour@beta.gouv.fr>
2023-03-09 11:45:58 +0100

    Rework routes & permissions

--- app/controllers/UserReportsFiltersController.scala
+++ app/controllers/UserReportsFiltersController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import com.mohiva.play.silhouette.api.Silhouette
+import models.UserPermission
 import models.report.ReportFilter
 import orchestrators.UserReportsFiltersOrchestrator
 import play.api.libs.json.JsError
@@ -11,6 +12,7 @@ import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.ControllerComponents
 import utils.silhouette.auth.AuthEnv
+import utils.silhouette.auth.WithPermission
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
@@ -27,7 +29,7 @@ class UserReportsFiltersController(
   implicit val userReportsFiltersRequestReads: Reads[UserReportsFiltersRequest] = Json.reads[UserReportsFiltersRequest]
 
   def save(): Action[JsValue] =
-    SecuredAction.async(parse.json) { implicit request =>
+    SecuredAction(WithPermission(UserPermission.crudUserReportsFilters)).async(parse.json) { implicit request =>
       request.body
         .validate[UserReportsFiltersRequest]
         .fold(
@@ -40,7 +42,7 @@ class UserReportsFiltersController(
     }
 
   def get(name: String): Action[AnyContent] =
-    SecuredAction.async { implicit request =>
+    SecuredAction(WithPermission(UserPermission.crudUserReportsFilters)).async { implicit request =>
       userReportsFiltersOrchestrator
         .get(request.identity.id, name)
         .map {
@@ -50,35 +52,35 @@ class UserReportsFiltersController(
     }
 
   def list(): Action[AnyContent] =
-    SecuredAction.async { implicit request =>
+    SecuredAction(WithPermission(UserPermission.crudUserReportsFilters)).async { implicit request =>
       userReportsFiltersOrchestrator
         .list(request.identity.id)
         .map(userReportsFiltersList => Ok(Json.toJson(userReportsFiltersList)))
     }
 
   def delete(name: String): Action[AnyContent] =
-    SecuredAction.async { implicit request =>
+    SecuredAction(WithPermission(UserPermission.crudUserReportsFilters)).async { implicit request =>
       userReportsFiltersOrchestrator
         .delete(request.identity.id, name)
         .map(_ => NoContent)
     }
 
   def rename(oldName: String, newName: String): Action[AnyContent] =
-    SecuredAction.async { implicit request =>
+    SecuredAction(WithPermission(UserPermission.crudUserReportsFilters)).async { implicit request =>
       userReportsFiltersOrchestrator
         .rename(request.identity.id, oldName, newName)
         .map(_ => NoContent)
     }
 
   def setAsDefault(name: String): Action[AnyContent] =
-    SecuredAction.async { implicit request =>
+    SecuredAction(WithPermission(UserPermission.crudUserReportsFilters)).async { implicit request =>
       userReportsFiltersOrchestrator
         .setAsDefault(request.identity.id, name)
         .map(_ => NoContent)
     }
 
   def unsetDefault(name: String): Action[AnyContent] =
-    SecuredAction.async { implicit request =>
+    SecuredAction(WithPermission(UserPermission.crudUserReportsFilters)).async { implicit request =>
       userReportsFiltersOrchestrator
         .unsetDefault(request.identity.id, name)
         .map(_ => NoContent)
