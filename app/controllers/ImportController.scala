3f69d00311607c46742c911c8244ad4d4c82ae78
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-01-08 17:31:02 +0100

    [TRELLO-1953] Some improvments

--- app/controllers/ImportController.scala
+++ app/controllers/ImportController.scala
@@ -12,14 +12,21 @@ import utils.EmailAddress
 import utils.SIREN
 import utils.SIRET
 import authentication.actions.UserAction.WithRole
+import models.company.AccessLevel
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
-case class ImportInput(siren: Option[SIREN], sirets: List[SIRET], emails: List[EmailAddress])
+case class ImportInput(
+    siren: Option[SIREN],
+    sirets: List[SIRET],
+    emails: List[EmailAddress],
+    onlyHeadOffice: Boolean,
+    level: AccessLevel
+)
 
 object ImportInput {
-  implicit val test: OFormat[ImportInput] = Json.format[ImportInput]
+  implicit val importInputFormat: OFormat[ImportInput] = Json.format[ImportInput]
 }
 
 class ImportController(
@@ -33,14 +40,14 @@ class ImportController(
     (input.siren, input.sirets, input.emails) match {
       case (_, _, Nil)             => Future.failed(EmptyEmails)
       case (None, Nil, _)          => Future.failed(EmptyEmails)
-      case (siren, sirets, emails) => Future.successful((siren, sirets, emails))
+      case (siren, sirets, emails) => Future.successful((siren, sirets, emails, input.onlyHeadOffice, input.level))
     }
 
   def importUsers = SecuredAction.andThen(WithRole(UserRole.Admin)).async(parse.json) { implicit request =>
     for {
-      importInput             <- request.parseBody[ImportInput]()
-      (siren, sirets, emails) <- validateInput(importInput)
-      _                       <- importOrchestrator.importUsers(siren, sirets, emails)
+      importInput                                    <- request.parseBody[ImportInput]()
+      (siren, sirets, emails, onlyHeadOffice, level) <- validateInput(importInput)
+      _ <- importOrchestrator.importUsers(siren, sirets, emails, onlyHeadOffice, level)
     } yield NoContent
   }
 }
