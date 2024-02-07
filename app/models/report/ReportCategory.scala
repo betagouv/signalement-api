6a0d68a5856749291f8fb8d9017e3b02874a1b24
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-02-07 17:54:17 +0100

    [TRELLO-2168] Implement a better 'lifecycle' for categories

--- app/models/report/ReportCategory.scala
+++ app/models/report/ReportCategory.scala
@@ -4,31 +4,47 @@ import controllers.error.AppError.MalformedValue
 import enumeratum.EnumEntry
 import enumeratum.PlayEnum
 
-sealed abstract class ReportCategory(val label: String, val legacy: Boolean = false) extends EnumEntry
+sealed trait ReportCategoryStatus
+object ReportCategoryStatus {
+  case object Legacy
+      extends ReportCategoryStatus // it has been modified / splitted / migrated and should never come back
+  case object Closed extends ReportCategoryStatus // Old category, not available and no investigation
+  case object Inactive
+      extends ReportCategoryStatus // Category not available in the website, but an investigation could still happen
+  case object Active extends ReportCategoryStatus
+}
+
+// Legacy means
+// Active means it's an old category not up to date anymore but could come back (COVID is a good example)
+sealed abstract class ReportCategory(val label: String, val status: ReportCategoryStatus = ReportCategoryStatus.Active)
+    extends EnumEntry
 
 object ReportCategory extends PlayEnum[ReportCategory] {
 
-  case object RetraitRappelSpecifique  extends ReportCategory("Retrait-Rappel spécifique")
-  case object Coronavirus              extends ReportCategory("COVID-19 (coronavirus)")
-  case object CafeRestaurant           extends ReportCategory("Café / Restaurant")
-  case object AchatMagasinLegacy       extends ReportCategory("Achat / Magasin", legacy = true)
-  case object AchatMagasinInternet     extends ReportCategory("Achat (Magasin ou Internet)", legacy = true)
-  case object AchatMagasin             extends ReportCategory("Achat en Magasin")
-  case object AchatInternet            extends ReportCategory("Achat sur internet")
-  case object ServicesAuxParticuliers  extends ReportCategory("Services aux particuliers")
-  case object TelEauGazElec            extends ReportCategory("Téléphonie / Eau-Gaz-Electricité", legacy = true)
-  case object EauGazElectricite        extends ReportCategory("Eau / Gaz / Electricité")
-  case object TelephonieFaiMedias      extends ReportCategory("Téléphonie / Fournisseur d'accès internet / médias")
-  case object BanqueAssuranceMutuelle  extends ReportCategory("Banque / Assurance / Mutuelle")
-  case object IntoxicationAlimentaire  extends ReportCategory("Intoxication alimentaire")
-  case object ProduitsObjets           extends ReportCategory("Produits / Objets", legacy = true)
-  case object Internet                 extends ReportCategory("Internet (hors achats)")
-  case object TravauxRenovations       extends ReportCategory("Travaux / Rénovation")
-  case object VoyageLoisirs            extends ReportCategory("Voyage / Loisirs")
-  case object Immobilier               extends ReportCategory("Immobilier")
-  case object Sante                    extends ReportCategory("Secteur de la santé")
-  case object VoitureVehicule          extends ReportCategory("Voiture / Véhicule", legacy = true)
-  case object Animaux                  extends ReportCategory("Animaux")
+  case object RetraitRappelSpecifique
+      extends ReportCategory("Retrait-Rappel spécifique", status = ReportCategoryStatus.Closed)
+  case object Coronavirus        extends ReportCategory("COVID-19 (coronavirus)", status = ReportCategoryStatus.Closed)
+  case object CafeRestaurant     extends ReportCategory("Café / Restaurant")
+  case object AchatMagasinLegacy extends ReportCategory("Achat / Magasin", status = ReportCategoryStatus.Legacy)
+  case object AchatMagasinInternet
+      extends ReportCategory("Achat (Magasin ou Internet)", status = ReportCategoryStatus.Legacy)
+  case object AchatMagasin            extends ReportCategory("Achat en Magasin")
+  case object AchatInternet           extends ReportCategory("Achat sur internet")
+  case object ServicesAuxParticuliers extends ReportCategory("Services aux particuliers")
+  case object TelEauGazElec
+      extends ReportCategory("Téléphonie / Eau-Gaz-Electricité", status = ReportCategoryStatus.Legacy)
+  case object EauGazElectricite       extends ReportCategory("Eau / Gaz / Electricité")
+  case object TelephonieFaiMedias     extends ReportCategory("Téléphonie / Fournisseur d'accès internet / médias")
+  case object BanqueAssuranceMutuelle extends ReportCategory("Banque / Assurance / Mutuelle")
+  case object IntoxicationAlimentaire extends ReportCategory("Intoxication alimentaire")
+  case object ProduitsObjets          extends ReportCategory("Produits / Objets", status = ReportCategoryStatus.Legacy)
+  case object Internet                extends ReportCategory("Internet (hors achats)")
+  case object TravauxRenovations      extends ReportCategory("Travaux / Rénovation")
+  case object VoyageLoisirs           extends ReportCategory("Voyage / Loisirs")
+  case object Immobilier              extends ReportCategory("Immobilier")
+  case object Sante                   extends ReportCategory("Secteur de la santé")
+  case object VoitureVehicule         extends ReportCategory("Voiture / Véhicule", status = ReportCategoryStatus.Legacy)
+  case object Animaux                 extends ReportCategory("Animaux")
   case object DemarchesAdministratives extends ReportCategory("Démarches administratives")
   case object VoitureVehiculeVelo      extends ReportCategory("Voiture / Véhicule / Vélo")
   case object DemarchageAbusif         extends ReportCategory("Démarchage abusif")
