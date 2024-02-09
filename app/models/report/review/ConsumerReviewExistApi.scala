4eb2abdf4428370445fc9bcc0f490074378cb82a
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-02-09 15:05:40 +0100

    [TRELLO-2185] Try to optimise and refacto

--- app/models/report/review/ConsumerReviewExistApi.scala
+++ app/models/report/review/ConsumerReviewExistApi.scala
@@ -1,12 +1,11 @@
 package models.report.review
 
 import play.api.libs.json.Json
-import play.api.libs.json.OFormat
+import play.api.libs.json.OWrites
 
-case class ConsumerReviewExistApi(
-    value: Boolean
-)
+case class ConsumerReviewExistApi(value: Boolean) extends AnyVal
 
 object ConsumerReviewExistApi {
-  implicit val consumerReviewExistApi: OFormat[ConsumerReviewExistApi] = Json.format[ConsumerReviewExistApi]
+  implicit val consumerReviewExistApi: OWrites[ConsumerReviewExistApi] =
+    (consumerReviewExistApi: ConsumerReviewExistApi) => Json.obj("value" -> consumerReviewExistApi.value)
 }
