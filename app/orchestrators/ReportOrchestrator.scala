fa10eb9d8d4f410860c2bfea9f4df484fe27dc1b
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-05-29 14:59:08 +0200

    Blacklist gmail now blacklist when . and + are the address

--- app/orchestrators/ReportOrchestrator.scala
+++ app/orchestrators/ReportOrchestrator.scala
@@ -259,7 +259,10 @@ class ReportOrchestrator(
       blacklistFromDb <- blacklistedEmailsRepository.list()
     } yield {
       val fullBlacklist = blacklistFromDb.map(_.email)
-      if (fullBlacklist.exists(blacklistedEmail => emailAddress.split.isEquivalentTo(blacklistedEmail)))
+      // Small optimisation to only computed the splitted email once
+      // Instead of multiple times in the exists loop
+      val splittedEmailAddress = emailAddress.split
+      if (fullBlacklist.exists(blacklistedEmail => splittedEmailAddress.isEquivalentTo(blacklistedEmail)))
         throw SpammerEmailBlocked(emailAddress)
       else ()
     }
