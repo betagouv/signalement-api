1e461305f5cf6e7a7aeb5670fc781a5b48acdcee
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2024-08-01 16:51:52 +0200

    TRELLO-2428: fix tests

--- test/utils/CompanySyncServiceMock.scala
+++ test/utils/CompanySyncServiceMock.scala
@@ -1,5 +1,6 @@
 package utils
 
+import models.company.Address
 import models.company.Company
 import tasks.company.CompanySearchResult
 import tasks.company.CompanySyncServiceInterface
@@ -11,7 +12,25 @@ class CompanySyncServiceMock extends CompanySyncServiceInterface {
   override def syncCompanies(companies: Seq[Company], lastUpdated: OffsetDateTime): Future[List[CompanySearchResult]] =
     Future.successful(List.empty[CompanySearchResult])
 
-  override def companyBySiret(siret: SIRET): Future[Option[CompanySearchResult]] = Future.successful(Option.empty)
+  override def companyBySiret(siret: SIRET): Future[Option[CompanySearchResult]] = Future.successful(
+    Some(
+      CompanySearchResult(
+        siret = siret,
+        name = None,
+        commercialName = None,
+        establishmentCommercialName = None,
+        brand = None,
+        isHeadOffice = false,
+        address = Address(),
+        activityCode = None,
+        activityLabel = None,
+        isMarketPlace = false,
+        isOpen = false,
+        isPublic = false,
+        lastUpdated = None
+      )
+    )
+  )
 
   override def companiesBySirets(sirets: List[SIRET]): Future[List[CompanySearchResult]] = Future.successful(List.empty)
 
