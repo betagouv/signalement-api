277dc4aa5699f7225b694e1efa55ea236d29f9c2
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-08-19 11:26:30 +0200

    [TRELLO-2477] Make gs1 product optional to always save GTIN (#1712)
    * [TRELLO-2477] Make gs1 product optional to always save GTIN
    
    * [TRELLO-2477] Make gs1 product optional to always save GTIN

--- app/orchestrators/BarcodeOrchestrator.scala
+++ app/orchestrators/BarcodeOrchestrator.scala
@@ -15,6 +15,7 @@ import services.OpenBeautyFactsServiceInterface
 import services.OpenFoodFactsServiceInterface
 import utils.Logs.RichLogger
 
+import java.time.OffsetDateTime
 import java.util.UUID
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
@@ -61,65 +62,68 @@ class BarcodeOrchestrator(
 
     } yield result
 
+  private def outdated(product: BarcodeProduct): Boolean = {
+    val oneMonthAgo = OffsetDateTime.now().minusMonths(1)
+    product.gs1Product.isEmpty && product.updateDate.isBefore(oneMonthAgo)
+  }
+
   def getByGTIN(gtin: String): Future[Option[BarcodeProduct]] =
     for {
       maybeExistingProductInDB <- barcodeRepository.getByGTIN(gtin)
       product <- maybeExistingProductInDB match {
+        case Some(oldProduct) if outdated(oldProduct) =>
+          for {
+            newProduct     <- fetchProductFromAPIs(gtin)
+            updatedProduct <- updateProduct(oldProduct = oldProduct, newProduct = newProduct)
+          } yield updatedProduct
         case Some(_) =>
           logger.debug(s"Fetched product with gtin $gtin from DB")
           Future.successful(maybeExistingProductInDB)
-        case None => fetchProductFromGS1(gtin)
+        case None =>
+          for {
+            newProduct     <- fetchProductFromAPIs(gtin)
+            createdProduct <- createProduct(newProduct)
+          } yield createdProduct
       }
     } yield product
 
-  private def fetchProductFromGS1(gtin: String): Future[Option[BarcodeProduct]] =
-    getFromGS1API(gtin).flatMap {
-      case Some(apiProduct) =>
-        val newProduct = BarcodeProduct(
-          gtin = gtin,
-          gs1Product = apiProduct,
-          openFoodFactsProduct = None,
-          openBeautyFactsProduct = None
-        )
-        barcodeRepository
-          .create(newProduct)
-          .flatMap { createdProduct =>
-            enrichWithOpenFactData(createdProduct).map(Some(_))
-          }
-          .recover { case error =>
-            logger.warnWithTitle(
-              "GS1_barcode_product_already_exists",
-              s"GTIN $gtin already exist cannot create it again",
-              error
-            )
-            None
-          }
-
-      case None =>
-        // GS1 API did not return any product
-        Future.successful(None)
-    }
-
-  /** Enrich BarcodeProduct after the creation to prevent calling open data api when multiple async call are made from
-    * all instances
-    */
-  private def enrichWithOpenFactData(barcodeProduct: BarcodeProduct): Future[BarcodeProduct] =
+  private def fetchProductFromAPIs(gtin: String): Future[BarcodeProduct] =
     for {
-      maybeProductFromOpenFoodFacts   <- openFoodFactsService.getProductByBarcode(barcodeProduct.gtin)
-      maybeProductFromOpenBeautyFacts <- openBeautyFactsService.getProductByBarcode(barcodeProduct.gtin)
-      updatedProduct <- {
-        val updatedProduct = barcodeProduct.copy(
-          openFoodFactsProduct = maybeProductFromOpenFoodFacts,
-          openBeautyFactsProduct = maybeProductFromOpenBeautyFacts
+      maybeGS1Product                 <- getFromGS1API(gtin)
+      maybeProductFromOpenFoodFacts   <- openFoodFactsService.getProductByBarcode(gtin)
+      maybeProductFromOpenBeautyFacts <- openBeautyFactsService.getProductByBarcode(gtin)
+    } yield BarcodeProduct(
+      gtin = gtin,
+      gs1Product = maybeGS1Product,
+      openFoodFactsProduct = maybeProductFromOpenFoodFacts,
+      openBeautyFactsProduct = maybeProductFromOpenBeautyFacts
+    )
+
+  private def createProduct(newProduct: BarcodeProduct): Future[Option[BarcodeProduct]] =
+    barcodeRepository
+      .create(newProduct)
+      .map(Some(_))
+      .recover { case error =>
+        logger.warnWithTitle(
+          "GS1_barcode_product_already_exists",
+          s"GTIN ${newProduct.gtin} already exist cannot create it again",
+          error
         )
-        // Update the product in the database if any additional data was found
-        if (maybeProductFromOpenFoodFacts.isDefined || maybeProductFromOpenBeautyFacts.isDefined) {
-          barcodeRepository.update(updatedProduct.id, updatedProduct).map(_ => updatedProduct)
-        } else {
-          Future.successful(updatedProduct)
-        }
+        None
       }
-    } yield updatedProduct
+
+  private def updateProduct(oldProduct: BarcodeProduct, newProduct: BarcodeProduct): Future[Option[BarcodeProduct]] =
+    barcodeRepository
+      .update(
+        oldProduct.id,
+        oldProduct.copy(
+          gs1Product = newProduct.gs1Product,
+          openFoodFactsProduct = newProduct.openFoodFactsProduct,
+          openBeautyFactsProduct = newProduct.openBeautyFactsProduct,
+          updateDate = OffsetDateTime.now()
+        )
+      )
+      .map(Some(_))
 
   def get(id: UUID): Future[Option[BarcodeProduct]] =
     barcodeRepository.get(id)
