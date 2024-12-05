eb036033c002e9e1bce4920d75a5f6c66e2b5f0f
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-12-05 14:58:34 +0200

    TRELLO-2773 Rendre les signalements de démo plus réalistes

---
+++ app/models/report/sampledata/SampleDescriptions.scala
@@ -0,0 +1,30 @@
+package models.report.sampledata
+
+object SampleDescriptions {
+
+  val sampleDescriptions = Seq(
+    // taken from real-life reports
+    // all numbers, brands, websites, etc. have been replaced by fake values
+    """J'ai demandé la portabilité de ma ligne mobile depuis l'opérateur Totoro vers youpla, j'ai recu la nouvelle carte sim avec un numéro provisoire, et par la suite un SMS m'indiquant : La portabilité du +336... vers youpla a été annulée pour une raison technique, demande de précisions au service client par chat sans réponse et impossible de les joindre autrement. si ma ligne sera résiliée avec l'ancien opérateur et que le nouvel opérateur me garde le numéro provisoir je serai dans une situation catastrophique, je ne peux pas perdre mon numéro qui j'ai depuis plus de 10 ans. AU SECOUR !!""",
+    """ras le bol trop c est trop""",
+    """Je fais référence à l'échange standard du radar TDR 3000 qui équipe mon hélicoptère, un Viper
+      |AP36, commandé en octobre 2013. Cet échange a été facturé au prix total de 70 058 € TTC,
+      |comme en atteste les factures n° 1158 et n° 2300.
+      |Malheureusement, depuis la réception de cet appareil, je constate avec regret que celui-ci ne
+      |fonctionne pas correctement.""".stripMargin,
+    """Nous avons sollicité MaCarteGrise.net pensant être sur un site officiel type ANTS. Nous avons déposé nos documents officiels sur la plate-forme. Nous avons réalisé qu’il s’agissait d’un intermédiaire. A ce stade, Nous n’avons pas notre carte grise et allons dépasser le délai d’un mois. Le site internet est à inaccessible et le téléphone 03 19 60 74 57 reste sur messagerie. Nous n’avons aucun moyen de joindre l’entreprise qui ne donne plus de nouvelles. Nous avons payé et téléchargé nos papiers d’identité et du véhicule sur ce site. Nous souhaitons remboursement de la prestation non réalisée 123,71€. Nous souhaitons savoir dans quelle mesure nos données personnelles sont sécurisées.""",
+    """Bonjour,
+      |
+      |Suite à une commande passée en magasin, et livraison programmé. J'ai reçu une table emballée en deux parties le 19/11/2024. L'emballage externe ne présentait aucun défaut.  En déballant je me retrouve avec un pied de table avec des moissisures réparties sur toute la surface. Je signale au service après vente dans les minutes qui suivent le problème. Un lien m'est envoyé par email pour télécharger des photos. Je signale au téléphone que le stockage de cette pièce à l'intérieur de mon domicile risque de contaminer le sol ou autre objet à mon domicile. On m'indique qu'il faut le conserver à mon domicile jusqu'au retour dans 48h de la part de leur service. Je signale qu'on est vendredi et qu'avec le week end le delais risque de s'allonger. On me dit de  suivre les consignes. Je conserve la table, et fais le nécessaire pour envoyer les photos le 19/11/2024, le 3/12/2024 je reçois un mail de leur service qui me propose un avoir contre le retour de la table. Je refuse par e-mail cette proposition car je ne souhaite plus passer commande chez cette enseigne. Suite à ma réponse le 3 décembre je n'ai plus aucun retour de leur part. Le problème étant assez sensible je les rappelle le 5 décembre en demandant à ce qu'on m'echange la table contre une nouvelle table. On me refuse ma demande en m'indiquant qu'il y a qu'une seule solution remboursement contre retour ou avoir contre retour. Aucune explication ne m'est donné sur les différents manquements de leur part:
+      |1) le manque contrôle qualité avant livraison
+      |2) il ne s'agit pas d'un défaut mais de moisissures qui peuvent porter atteinte à la sécurité du client. En effet, je me retrouve à stocker l'objet moisi jusqu'au moment où le retour est planifié. D'autres surface peuvent se contaminer à mon domicile. Le délai de 48h du service après vente n'a pas été respecté. Mon e-mail du 3 décembre est resté sans réponse. J'ai du les rappeler moi même le 5 décembre pour relancer.
+      |3)Déco Plus n'a pas jugé urgent ma demande malgres que cela ne soit pas un défaut technique mais des moisissures
+      |4) aucun effort du service client pour convenir d'une solution rapidement.
+      |J'ai du constamment les relancer. A ce jour je me retrouve contrainte à accepter un remboursement, aucune écoute de leur part concernant  le stress que cela a engendré.
+      |J'ai tenu mes engaments (paiement réalisé, patienté pendant plus d'un mois pour la livraison, honorer le rdv de livraison, fait la réclamation dans les minutes de la reception) mais maison du monde n'a pas respecté ses engaments sur toute la ligne.
+      |
+      |Je trouve cela inadmissible de recevoir un objet moisi. Et qu'on ai à le garder pendant plus de 6 jours en risquant de contaminer d'autres objets, et d'augmenter les risques pour sa propre santé.
+      |C'est au client de relancer à plusieurs reprises le service réclamation pour trouver une solution. A aucun niveau deco Plus apporte un support au client pour trouver une solution.""".stripMargin
+  )
+
+}
