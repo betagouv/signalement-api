c57b2b56b193816815d44f498071661902dda347
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2025-02-11 14:59:24 +0200

    renaming

--- app/controllers/CompanyAccessController.scala
+++ app/controllers/CompanyAccessController.scala
@@ -244,7 +244,7 @@ class CompanyAccessController(
       )
   }
 
-  def proFirstActivationCount(ticks: Option[Int]) = Act.secured.all.allowImpersonation.async(parse.empty) { _ =>
+  def getProFirstActivationCount(ticks: Option[Int]) = Act.secured.all.allowImpersonation.async(parse.empty) { _ =>
     accessesOrchestrator.proFirstActivationCount(ticks).map(x => Ok(Json.toJson(x)))
   }
 
