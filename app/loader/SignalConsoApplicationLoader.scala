9fe767dd0240da6b9a7f52e3af589206f0ea4805
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2025-03-15 07:22:47 +0100

    TRELLO-2852 : Fix compilation issues

--- app/loader/SignalConsoApplicationLoader.scala
+++ app/loader/SignalConsoApplicationLoader.scala
@@ -419,6 +419,22 @@ class SignalConsoComponents(
   def antivirusService: AntivirusServiceInterface =
     new AntivirusService(conf = signalConsoConfiguration.antivirusServiceConfiguration, backend)
 
+  val reportWithDataOrchestrator =
+    new ReportWithDataOrchestrator(
+      visibleReportOrchestrator,
+      companyRepository,
+      eventRepository,
+      reportFileRepository,
+      responseConsumerReviewRepository,
+      reportEngagementReviewRepository
+    )
+
+  val reportZipExportService =
+    new ReportZipExportService(htmlFromTemplateGenerator, pdfService, s3Service, reportWithDataOrchestrator)(
+      materializer,
+      actorSystem
+    )
+
   val reportFileOrchestrator =
     new ReportFileOrchestrator(
       reportFileRepository,
@@ -479,21 +495,7 @@ class SignalConsoComponents(
     eventRepository
   )
 
-  val reportWithDataOrchestrator =
-    new ReportWithDataOrchestrator(
-      visibleReportOrchestrator,
-      companyRepository,
-      eventRepository,
-      reportFileRepository,
-      responseConsumerReviewRepository,
-      reportEngagementReviewRepository
-    )
 
-  val reportZipExportService =
-    new ReportZipExportService(htmlFromTemplateGenerator, pdfService, s3Service, reportWithDataOrchestrator)(
-      materializer,
-      actorSystem
-    )
 
   val socialBladeClient      = new SocialBladeClient(applicationConfiguration.socialBlade)
   val influencerOrchestrator = new InfluencerOrchestrator(influencerRepository, socialBladeClient)
@@ -885,8 +887,7 @@ class SignalConsoComponents(
     controllerComponents,
     reportWithDataOrchestrator,
     visibleReportOrchestrator,
-    reportZipExportService,
-    htmlFromTemplateGenerator
+    reportZipExportService
   )
 
   val reportedPhoneController = new ReportedPhoneController(
