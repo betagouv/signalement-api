d38ae629d199c0ec8520b27732595f14d7013b79
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2025-03-31 08:41:40 +0200

    master : fix resource path

--- app/utils/B64PdfRessource.scala
+++ app/utils/B64PdfRessource.scala
@@ -8,26 +8,26 @@ import scala.io.BufferedSource
 object B64PdfRessource {
 
   val ReportCss: String = {
-    val source: BufferedSource = scala.io.Source.fromFile("public/css/report.css")
+    val source: BufferedSource = scala.io.Source.fromFile("css/report.css")
     val css                    = source.mkString
     source.close()
     css
   }
 
   val LogoMinister: String = {
-    val imgBytes = Files.readAllBytes(Paths.get("public/images/logo-ministere.jpg"))
+    val imgBytes = Files.readAllBytes(Paths.get("images/logo-ministere.jpg"))
     val base64   = Base64.getEncoder.encodeToString(imgBytes)
     s"data:image/jpg;base64,$base64"
   }
 
   val LogoSignalConso: String = {
-    val imgBytes = Files.readAllBytes(Paths.get("public/images/logo-signal-conso.png"))
+    val imgBytes = Files.readAllBytes(Paths.get("images/logo-signal-conso.png"))
     val base64   = Base64.getEncoder.encodeToString(imgBytes)
     s"data:image/png;base64,$base64"
   }
 
   val LogoMarianne: String = {
-    val imgBytes = Files.readAllBytes(Paths.get("public/images/logo-marianne.jpg"))
+    val imgBytes = Files.readAllBytes(Paths.get("images/logo-marianne.jpg"))
     val base64   = Base64.getEncoder.encodeToString(imgBytes)
     s"data:image/jpg;base64,$base64"
   }
